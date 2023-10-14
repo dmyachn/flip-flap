b0221f6431ab5b1034fa3fe5398d975b9c3e66bc
Dmitry <d.myachn@gmail.com>
2023-10-14 23:33:45 +0300

    done

--- flip-flap/ContentView.swift
+++ flip-flap/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    var ResizableRect: some View {
+        return Rectangle()
+            .clipShape(.rect(cornerRadius: 7, style: .continuous))
+            .scaledToFit()
+            .foregroundColor(.cyan)
+    }
+    
+    
+    var count = 6
+    @State var isVertical = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let layout = isVertical ? AnyLayout(SlashStack(spacing: 20)) : AnyLayout(HStackLayout(spacing: 20))
+        
+        layout{
+            ForEach(0..<count, id: \.self) { index in
+                ResizableRect
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            isVertical.toggle()
+                        }
+                    }
+            }
         }
-        .padding()
+    }
+}
+
+struct SlashStack: Layout {
+    var spacing: CGFloat = 10
+    
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        proposal.replacingUnspecifiedDimensions()
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        var x = bounds.minX
+        var y = bounds.maxY
+        
+        for index in subviews.indices {
+            
+            let rectSize =  (max(proposal.replacingUnspecifiedDimensions().height, proposal.replacingUnspecifiedDimensions().width) - spacing * CGFloat(subviews.count - 1)) / CGFloat(subviews.count)
+            
+            let sizeProposal = ProposedViewSize(
+                width: rectSize,
+                height: rectSize)
+            
+            subviews[index]
+                .place(
+                    at: CGPoint(x: x, y: y),
+                    anchor: .bottomLeading,
+                    proposal: sizeProposal
+                )
+            if proposal.replacingUnspecifiedDimensions().height > proposal.replacingUnspecifiedDimensions().width {
+                x += rectSize - (proposal.replacingUnspecifiedDimensions().height - proposal.replacingUnspecifiedDimensions().width) / CGFloat(subviews.count - 1) + spacing
+                y -= rectSize + spacing
+            } else {
+                x += rectSize + spacing
+                y -= rectSize - (proposal.replacingUnspecifiedDimensions().width - proposal.replacingUnspecifiedDimensions().height) / CGFloat(subviews.count - 1) + spacing
+            }
+        }
     }
 }
 
