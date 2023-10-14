4e202614afa5fe0d73a229637d798fb578c4551d
Dmitry <d.myachn@gmail.com>
2023-10-14 16:25:08 +0300

    Initial Commit

---
+++ flip-flap/flip_flapApp.swift
@@ -0,0 +1,17 @@
+//
+//  flip_flapApp.swift
+//  flip-flap
+//
+//  Created by Dmitiy Myachin on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct flip_flapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
